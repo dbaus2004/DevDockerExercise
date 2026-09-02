f676c4280e7cbb3ae2cf598c9a0afadec97262d9
dbaus2004 <dbaus2004@gmail.com>
2026-09-02 23:21:06 +0000

    modified server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,16 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//var express = require('express');
+import express from 'express';
+//var path = require('path');
+import path from 'path';
+//var cookieParser = require('cookie-parser');
+import cookieParser from 'cookie-parser';
+//var logger = require('morgan');
+import logger from 'morgan';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var indexRouter = require('./routes/index');
+import indexRouter from './routes/index.js';
+//var usersRouter = require('./routes/users');
+import usersRouter from './routes/users.js';
 
 var app = express();
 
@@ -12,9 +18,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(import.meta.dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+//module.exports = app;
+export default app;
